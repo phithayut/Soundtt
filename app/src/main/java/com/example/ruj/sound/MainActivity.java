08688dae283e399b8ce7c2891feff850ca832cd3
phithayut <eng_z_16907@hotmail.com>
2016-12-06 09:29:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ruj/sound/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.ruj.sound;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView play = (ImageView)findViewById(R.id.play_one1_2);
+        ImageView stop = (ImageView)findViewById(R.id.stop_one1_2);
+        final MediaPlayer sound = MediaPlayer.create(MainActivity.this,R.raw.cc);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sound.start();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sound.pause();
+            }
+        });
+    }
+}
